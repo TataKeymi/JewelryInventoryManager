13b526ecaf2099883d889665229a0925c4f59067
unknown <tanakutova74@gmail.com>
2026-09-19 13:40:51 +0300

    Serialize inventory items to JSON

--- storage.py
+++ storage.py
@@ -4,8 +4,21 @@ from models import JewelryItem, Ring, Earrings
 
 
 def save_inventory(items, filename):
+    items_to_save = []
+    for item in items:
+        item_data = {
+            "name": item.name,
+            "material": item.material,
+            "price": item.price,
+            "quantity": item.quantity
+        }
+        if isinstance(item, Ring):
+            item_data["size"] = item.size
+        elif isinstance(item, Earrings):
+            item_data["fastening_type"] = item.fastening_type
+        items_to_save.append(item_data)
     with open(filename, "w", encoding="utf-8") as file:
-        json.dump(items, file, indent=4)
+        json.dump(items_to_save, file, indent=4)
 
 
 def load_inventory(filename):
